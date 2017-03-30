29b5b8ae34c1f61970ee697a52f17c5601396859
pokutuna <popopopopokutuna@gmail.com>
2017-03-30 12:09:26 +0900

    use project's tsserver if exists

--- inits/53-typescript.el
+++ inits/53-typescript.el
@@ -13,4 +13,14 @@
     (setq flycheck-check-syntax-automatically '(save mode-enabled))
     (eldoc-mode t)
     (company-mode +1))
-  (add-hook 'typescript-mode-hook 'my-ts-mode-hook))
+  (add-hook 'typescript-mode-hook 'my-ts-mode-hook)
+
+  ;; use project's tsserver if exists
+  (defun my-set-project-tsserver ()
+    (let ((project-tsserver-path (concat tide-project-root "node_modules/typescript/bin/tsserver")))
+      (when (file-exists-p project-tsserver-path)
+        (setq-local tide-tsserver-executable project-tsserver-path))
+      ))
+  (advice-add 'tide-start-server :before 'my-set-project-tsserver)
+
+  )
